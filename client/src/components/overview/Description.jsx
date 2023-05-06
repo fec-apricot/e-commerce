73ea3372dbc8ad9e64b9a07e859d4f5517a388c0
XintongTheCoder <xintongmi@gmail.com>
2023-05-05 23:08:26 -0700

    overview components set up

---
+++ client/src/components/overview/Description.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+function Description() {
+  const DescriptionContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+  `;
+  return <div>HEllo!!</div>;
+}
+
+export default Description;
---
+++ client/src/components/overview/ImageGallery.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+function ImageGallery() {
+  return <div>HEllo!!</div>;
+}
+
+export default ImageGallery;
