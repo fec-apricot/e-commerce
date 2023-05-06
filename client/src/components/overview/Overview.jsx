13df82417e39354ae2ced9bcb65f39bea1507b0a
XintongTheCoder <xintongmi@gmail.com>
2023-05-06 12:10:07 -0700

    set up StyleContext in Overview.jsx

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import parse from '../../parse.js';
 import styled from 'styled-components';
 import ImageGallery from './ImageGallery.jsx';
@@ -6,13 +6,25 @@ import Description from './Description.jsx';
 import StyleSelector from './StyleSelector.jsx';
 import AddToCart from './AddToCart.jsx';
 
+const StyleContext = createContext();
+
 function Overview({ productID }) {
   const [product, setProduct] = useState({});
+  const [styles, setStyles] = useState([]);
+  const [currentStyle, setCurrentStyle] = useState({});
+
   useEffect(() => {
     parse
       .get(`http://localhost:3000/products/${productID}`)
       .then((data) => {
         setProduct(data);
+      })
+      .then(() =>
+        parse.get(`http://localhost:3000/products/${productID}/styles`)
+      )
+      .then((data) => {
+        setStyles(data.results);
+        setCurrentStyle(data.results[0]);
       })
       .catch((err) => {
         console.error(err.message);
@@ -37,20 +49,22 @@ function Overview({ productID }) {
   const Price = styled.div``;
 
   return (
-    <Host>
-      <TopContainer>
-        <ImageGallery></ImageGallery>
-        <WidgetPanel>
-          {/* <RatingContainer></RatingContainer> */}
-          <Category>{product.category}</Category>
-          <Name>{product.name}</Name>
-          <Price>{product.price}</Price>
-          <StyleSelector></StyleSelector>
-          <AddToCart></AddToCart>
-        </WidgetPanel>
-      </TopContainer>
-      <Description></Description>
-    </Host>
+    <StyleContext.Provider value={(currentStyle, styles)}>
+      <Host>
+        <TopContainer>
+          <ImageGallery></ImageGallery>
+          <WidgetPanel>
+            {/* <RatingContainer></RatingContainer> */}
+            <Category>{product.category}</Category>
+            <Name>{product.name}</Name>
+            <Price>${product.default_price}</Price>
+            <StyleSelector></StyleSelector>
+            <AddToCart></AddToCart>
+          </WidgetPanel>
+        </TopContainer>
+        <Description></Description>
+      </Host>
+    </StyleContext.Provider>
   );
 }
 
