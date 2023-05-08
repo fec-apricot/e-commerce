336e871abf59f067e1afd2a4af82418490f4e436
XintongTheCoder <xintongmi@gmail.com>
2023-05-08 10:39:33 -0700

    refactor App.jsx with useContext

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,30 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import Overview from './overview/Overview.jsx';
-import RelatedProducts from './related_products/RelatedProducts.jsx';
-import Questions from './questions/Questions.jsx';
-import Reviews from './reviews/Reviews.jsx';
-import parse from '../parse.js';
+import React, { useState, useEffect, createContext } from "react";
+import Overview from "./overview/Overview.jsx";
+import RelatedProducts from "./related_products/RelatedProducts.jsx";
+import Questions from "./questions/Questions.jsx";
+import Reviews from "./reviews/Reviews.jsx";
+import parse from "../parse.js";
 
+export const ProductIDContext = createContext();
 
-function App () {
+function App() {
   const [productID, setProductID] = useState(40344);
 
-
-
   useEffect(() => {
-    parse.get('/products/' + productID);
+    parse.get("/products/" + productID);
   }, []);
 
   return (
-    <div>
+    <ProductIDContext.Provider value={{ productID, setProductID }}>
       <div>HEllo!!</div>
-      <Overview productID={productID} />
-      <RelatedProducts productID={productID}  setProductID={setProductID}/>
-      <Questions productID={productID} />
-      <Reviews productID={productID} />
-    </div>
-  )
+      <Overview />
+      <RelatedProducts />
+      <Questions />
+      <Reviews />
+    </ProductIDContext.Provider>
+  );
 }
-
 
 export default App;
