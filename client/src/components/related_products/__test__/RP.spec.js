e1531f2f648e5a558a5fbb748ef4a5a529e3f0e1
jtoneill <tyleroneill21@gmail.com>
2023-05-09 16:45:05 -0700

    Trying to get tests to work.

---
+++ client/src/components/related_products/__test__/RP.spec.js
@@ -0,0 +1,4 @@
+import { render, screen } from '@testing-library/react';
+import RelatedProducts from '../RelatedProducts.jsx';
+
+it('Should render')
---
+++ client/src/components/related_products/stars_module/__test__/Stars.spec.js
@@ -0,0 +1,11 @@
+/* eslint-env jest */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Stars from '../Stars.jsx';
+
+describe('Stars module', () => {
+  test('Should render', async () => {
+    expect(true).toBe(true);
+    // const starsModule = screen.getByClas
+  });
+});
