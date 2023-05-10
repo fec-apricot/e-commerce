9829b6d1f9c4fcc6869ef3d77734f6b22bab9ba8
Noriel <flores1noriel@gmail.com>
2023-05-10 12:02:42 -0700

    Turned off prop types in ESLint config

--- tests/test.review/review.spec.js
+++ tests/test.review/review.spec.js
@@ -1,9 +1,9 @@
 /* eslint-env jest */
-import { render, screen, cleanup } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
 import Reviews from '../../client/src/components/reviews/Reviews.jsx';
 import { GlobalContextProvider } from '../../client/src/components/GlobalContext.jsx';
 import '@testing-library/jest-dom/'
-import React from 'react'
 
 test('test', () => {
   expect(true).toBe(true);
@@ -13,8 +13,8 @@ test('renders sort text button', async () => {
   render(
     <GlobalContextProvider>
       <Reviews />
-    </GlobalContextProvider>
+    </GlobalContextProvider>,
   );
   const linkElement = await screen.getByText(/More Reviews/);
   expect(linkElement).toBeInTheDocument();
-})
+});
