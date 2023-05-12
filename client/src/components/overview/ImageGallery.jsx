b8f36feb84771615b1fb10524b45743266e37b84
XintongTheCoder <xintongmi@gmail.com>
2023-05-12 10:39:41 -0700

    start working in image gallery

--- client/src/components/overview/ImageGallery.jsx
+++ client/src/components/overview/ImageGallery.jsx
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import styled from 'styled-components';
 import { OverviewContext } from './OverviewContext.jsx';
 
 const Host = styled.div`
+  position: relative;
   width: 65%;
   height: 100%;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
@@ -19,13 +20,115 @@ const DefaultView = styled.img`
   object-fit: contain;
 `;
 
+const ThumbnailViewContainer = styled.div`
+  position: absolute;
+  top: 45px;
+  left: 10px;
+  width: 90px;
+  height: 600px;
+  display: felx;
+  flex-direction: column;
+  justify-containt: space-between;
+`;
+
+const ThumbnailView = styled.img`
+  width: 80px;
+  height: 80px;
+  object-fit: contain;
+`;
+
+const Button = styled.button`
+  position: absolute;
+  &.prev-left {
+    top: 50%;
+    left: 100px;
+  }
+  &.next-right {
+    top: 50%;
+    right: 30px;
+  }
+  &.prev-up {
+    top: 20px;
+    left: 15px;
+  }
+  &.next-down {
+    bottom: 20px;
+    left: 15px;
+  }
+`;
+
 function ImageGallery() {
   const { selectedStyle } = useContext(OverviewContext);
+  const { photos } = selectedStyle;
+  const [defaultViewIndex, setDefaultViewIndex] = useState(0);
+  const [thumbnailViewIndexStart, setThumbnailIndexStart] = useState(0);
+  const MAXTHUMBNAILVIEWLENGTH = 3;
 
-  const defaultPhoto = selectedStyle.photos;
+  const handleScroll = (direction, setIndex) => {
+    setIndex((prevIndex) => prevIndex + direction);
+  };
+
   return (
     <Host>
-      <DefaultView src={defaultPhoto && defaultPhoto[0]?.url} />
+      <DefaultView src={photos && photos[defaultViewIndex]?.url} />
+      {defaultViewIndex > 0
+      && (
+        <Button
+          className="prev-left"
+          onClick={() => {
+            handleScroll(-1, setDefaultViewIndex);
+          }}
+        >
+          ←
+        </Button>
+      )}
+      {defaultViewIndex < photos?.length - 1
+      && (
+        <Button
+          className="next-right"
+          onClick={() => {
+            handleScroll(1, setDefaultViewIndex);
+          }}
+        >
+          →
+        </Button>
+      )}
+      <ThumbnailViewContainer>
+        {photos?.slice(
+          thumbnailViewIndexStart,
+          Math.min(
+            photos.length,
+            thumbnailViewIndexStart + MAXTHUMBNAILVIEWLENGTH,
+          ),
+        ).map((photo) => (
+          <ThumbnailView
+            src={photo.thumbnail_url}
+            key={photo.thumbnail_url}
+          />
+        ))}
+      </ThumbnailViewContainer>
+      {thumbnailViewIndexStart > 0
+      && (
+        <Button
+          className="prev-up"
+          onClick={() => {
+            handleScroll(-1, setThumbnailIndexStart);
+          }}
+        >
+          ←
+        </Button>
+      )}
+      {thumbnailViewIndexStart + MAXTHUMBNAILVIEWLENGTH < photos?.length
+      && (
+        <Button
+          className="next-down"
+          onClick={() => {
+            handleScroll(1, setThumbnailIndexStart);
+          }}
+        >
+          →
+        </Button>
+      )}
     </Host>
   );
 }
