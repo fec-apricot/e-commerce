dd1f85dbe2e01cb45507ae0a51fbd92de83d4a2e
jtoneill <tyleroneill21@gmail.com>
2023-05-12 16:13:53 -0700

    Figured out how to fire user events in my tests!!!

--- client/src/components/related_products/stars_module/__test__/Stars.spec.js
+++ client/src/components/related_products/stars_module/__test__/Stars.spec.js
@@ -1,47 +1,81 @@
-// /* eslint-env jest */
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import Stars from '../Stars.jsx';
-// import '@testing-library/jest-dom';
+/* eslint-env jest */
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Stars from '../Stars.jsx';
+import '@testing-library/jest-dom';
 
-// describe('Stars module', () => {
-//   test('Should say true is true', async () => {
-//     expect(true).toBe(true);
-//   });
+describe('Stars module Non-Interactive', () => {
+  it('Should say true is true', async () => {
+    expect(true).toBe(true);
+  });
 
-//   test('Should render stars to the screen', async () => {
-//     render(
-//       <Stars
-//         ratings={{
-//           1: '60',
-//           2: '60',
-//           3: '60',
-//           4: '600',
-//           5: '60',
-//         }}
-//         size={20}
-//         interactive={false}
-//       />,
-//     );
-//     const starsOnScreen = screen.getByText(/★★★★★/i);
-//     expect(starsOnScreen).toBeVisible();
-//   });
+  beforeEach(() => {
+    render(
+      <Stars
+        ratings={{
+          1: '60',
+          2: '60',
+          3: '60',
+          4: '600',
+          5: '60',
+        }}
+        size={20}
+        interactive={false}
+        cb={() => {}}
+      />,
+    );
+  });
 
-//   test('Should render stars to the screen', async () => {
-//     render(
-//       <Stars
-//         ratings={{
-//           1: '60',
-//           2: '60',
-//           3: '60',
-//           4: '600',
-//           5: '60',
-//         }}
-//         size={20}
-//         interactive={false}
-//       />,
-//     );
-//     const starsOnScreen2 = screen.getByTestId(/theStars/i);
-//     expect(starsOnScreen2).toBeInTheDocument();
-//   });
-// });
+  it('Should render stars to the screen', async () => {
+    const starsOnScreen = screen.getByText(/★★★★★/i);
+    expect(starsOnScreen).toBeVisible();
+  });
+
+  it('Should find element with data-testid rendered on the screen', async () => {
+    const starsOnScreen = screen.getByTestId(/theStars/i);
+    expect(starsOnScreen).toBeInTheDocument();
+  });
+});
+
+describe('Stars module', () => {
+  it('Should say true is true', async () => {
+    expect(true).toBe(true);
+  });
+
+  beforeEach(() => {
+    render(
+      <Stars
+        ratings={{
+          1: '60',
+          2: '60',
+          3: '60',
+          4: '600',
+          5: '60',
+        }}
+        size={20}
+        interactive
+        cb={() => {}}
+      />,
+    );
+  });
+
+  it('Should update stars classNames when hovered', async () => {
+    const fourthStar = screen.getByTestId(/star4/i);
+    fireEvent.mouseOver(fourthStar);
+    expect(fourthStar.classList.contains('gold')).toBe(true);
+  });
+
+  it('Should update classNames when hovered', async () => {
+    const fourthStar = screen.getByTestId(/star4/i);
+    const secondStar = screen.getByTestId(/star2/i);
+    fireEvent.mouseOver(fourthStar);
+    expect(secondStar.classList.contains('gold')).toBe(true);
+  });
+
+  it('Should update classNames when hovered', async () => {
+    const fourthStar = screen.getByTestId(/star4/i);
+    const secondStar = screen.getByTestId(/star2/i);
+    fireEvent.mouseOver(secondStar);
+    expect(fourthStar.classList.contains('gold')).toBe(false);
+  });
+});
